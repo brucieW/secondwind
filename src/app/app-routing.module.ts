b153aafe4e3a066de5fd7a825f7f32f87dc39b7a
brucieW <bruciew50@gmail.com>
2021-04-05 14:36:38 +1000

    Displaying main menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {PerformancesComponent} from './performances/performances.component';
+import {PhotoGalleryComponent} from './photo-gallery/photo-gallery.component';
+import {SponsorsComponent} from './sponsors/sponsors.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {MusicalDirectorsComponent} from './musical-directors/musical-directors.component';
+import {MemberNewsComponent} from './member-news/member-news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'performances',
+    component: PerformancesComponent
+  },
+  {
+    path: 'photos',
+    component: PhotoGalleryComponent
+  },
+  {
+    path: 'sponsors',
+    component: SponsorsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactUsComponent
+  },
+  {
+    path: 'musical-directors',
+    component: MusicalDirectorsComponent
+  },
+  {
+    path: 'member-news',
+    component: MemberNewsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PerformancesComponent } from './performances/performances.component';
@@ -12,6 +13,15 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { MusicalDirectorsComponent } from './musical-directors/musical-directors.component';
 import { MemberNewsComponent } from './member-news/member-news.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { RootNavComponent } from './root-nav/root-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -24,11 +34,21 @@ import { SideBarComponent } from './side-bar/side-bar.component';
     ContactUsComponent,
     MusicalDirectorsComponent,
     MemberNewsComponent,
-    SideBarComponent
+    SideBarComponent,
+    RootNavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    FlexLayoutModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
